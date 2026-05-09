ed8306b3de2839cf9dd3cf14cb8e0d0cb8017026
Imran <imranbiswas9299@gmail.com>
2026-05-09 22:03:02 +0600

    Add navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,85 +1,97 @@
+import { IoNotificationsSharp } from "react-icons/io5";
 import logo from "../../assets/Logo/closer_logo.png";
 import { BiSearch } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="">
-      <div className="navbar shadow-md bg-base-100 border-b border-gray-300">
-        <div className="navbar-start relative">
-          <img className="w-15 absolute" src={logo} alt="" />
-          <h1 className=" text-2xl ml-12 font-bold ">CLOSER</h1>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
+    <div className="shadow-md bg-base-100 border-b border-gray-300">
+      <div className="lg:mx-5">
+        <div className="navbar">
+          <div className="navbar-start relative w-full">
+            <img className="w-15 absolute" src={logo} alt="" />
+            <h1 className=" text-2xl ml-12 font-bold ">CLOSER</h1>
 
-        <div className="navbar-end">
-          <div className="btn btn-circle">
-            <BiSearch size={24} />
+            <div className="ml-5 w-full hidden md:block">
+              <label className="input">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </g>
+                </svg>
+                <input type="search" className="grow " placeholder="Search" />
+                <kbd className="kbd kbd-sm">⌘</kbd>
+                <kbd className="kbd kbd-sm">K</kbd>
+              </label>
+            </div>
           </div>
           
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={-1}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
 
-          {/* <div className="dropdown dropdown-end">
+          <div className="navbar-end ">
+            <div className="btn btn-circle hidden lg:flex items-center mr-3">
+              <IoNotificationsSharp size={28} />
+            </div>
+            {/* Mobile view */}
+            <div className="btn btn-circle lg:hidden">
+              <BiSearch size={24} />
+            </div>
+
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={-1}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="dropdown dropdown-end hidden lg:block lg:mr-5">
               <div
                 tabIndex={0}
                 role="button"
@@ -109,7 +121,8 @@ const Navbar = () => {
                   <a>Logout</a>
                 </li>
               </ul>
-            </div> */}
+            </div>
+          </div>
         </div>
       </div>
     </div>
